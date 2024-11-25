8b657c1ba7b46e91dababab711c51d9c0f268dff
JonasMarin2 <166415812+JonasMarin2@users.noreply.github.com>
2024-11-25 10:24:37 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -8,32 +8,32 @@ function escavarFosil(nivel) {
 }
 
 function mostrarResultadoEscavacao() {
-    const nivel = document.getElementById('nivel').value
-    const resultado = escavarFosil(nivel)
+    let nivel = document.getElementById('nivel').value
+    let resultado = escavarFosil(nivel)
     document.getElementById('resultado-escavacao').innerHTML = resultado
 }
 
 function converterIdadeCanina(idadeHumana) {
-    const fatorConversao = 7; // 1 ano humano = 7 anos caninos
+    let fatorConversao = 7; 
     return idadeHumana * fatorConversao;
 }
 
 function mostrarResultadoIdadeCanina() {
-    const idadeHumana = document.getElementById('idadeHumana').value;
-    const resultado = converterIdadeCanina(idadeHumana);
+    let idadeHumana = document.getElementById('idadeHumana').value;
+    let resultado = converterIdadeCanina(idadeHumana);
     document.getElementById('resultado-idade-canina').innerHTML = `A idade equivalente em anos caninos é: ${resultado} anos.`;
 }
 
 
 function calcularCrescimentoPopulacional(populacao, taxa) {
-    const crescimento = populacao * (taxa / 100); // Taxa em formato decimal
+    let crescimento = populacao * (taxa / 100); // Taxa em formato decimal
     return populacao + crescimento;
 }
 
 function calcularDesconto() {
-    const preco = parseFloat(document.getElementById('preco').value);
-    const desconto = parseFloat(document.getElementById('desconto').value);
-    const valorDesconto = preco * (desconto / 100);
-    const precoFinal = preco - valorDesconto;
+    let preco = parseFloat(document.getElementById('preco').value);
+    let desconto = parseFloat(document.getElementById('desconto').value);
+    let valorDesconto = preco * (desconto / 100);
+    let precoFinal = preco - valorDesconto;
     document.getElementById('resultado').innerHTML = `Preço final com desconto: R$ ${precoFinal.toFixed(2)}`;
 }
