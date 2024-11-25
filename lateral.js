846630a428f918c57b16c4ba700c168858854222
JonasMarin2 <166415812+JonasMarin2@users.noreply.github.com>
2024-11-25 10:25:13 -0300

    Update lateral.js

--- lateral.js
+++ lateral.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const menuBtn = document.getElementById('menu-btn');
-    const sidebar = document.getElementById('sidebar');
+    let menuBtn = document.getElementById('menu-btn');
+    let sidebar = document.getElementById('sidebar');
     menuBtn.addEventListener('click', function () {
         if (sidebar.style.left === "-200px") {
             sidebar.style.left = "0";
